f3de8675c5293d498d8f2b81ab24cbd110c592d2
Eriks Briedis <eriks.briedis@gmail.com>
2023-11-07 22:19:33 +0200

    config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: process.env.API_URL,
+    API_URL: process.env.API_URL || 'https://172-232-134-21.ip.linodeusercontent.com', // @TODO: temporary
   },
   transpilePackages: ["ui", "shared"],
 }
