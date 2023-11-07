bb4cbe104549285a9dd7f2b56cc09bcc6442cc8f
Eriks Briedis <eriks.briedis@gmail.com>
2023-11-07 11:54:07 +0200

    knex config

--- apps/api/src/db/knexfile.ts
+++ apps/api/src/db/knexfile.ts
@@ -5,7 +5,13 @@ const knex = require('knex')
 
 export const knexConfig: Knex.Config = {
   client: 'pg',
-  connection: process.env.DB_CONNECTION_STRING,
+  connection: {
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432'),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || 'apartconnect',
+  },
   migrations: {
     extension: 'ts',
     directory: 'src/db/migrations',
