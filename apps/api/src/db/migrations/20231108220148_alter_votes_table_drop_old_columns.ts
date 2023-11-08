ed25706fe86031fe56e51a91e2d09efbc5ed72b1
Eriks Briedis <eriks.briedis@gmail.com>
2023-11-09 00:04:48 +0200

    fix migration

--- apps/api/src/db/migrations/20231108220148_alter_votes_table_drop_old_columns.ts
+++ apps/api/src/db/migrations/20231108220148_alter_votes_table_drop_old_columns.ts
@@ -5,6 +5,9 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.table('vote', (table) => {
     table.dropColumn('value')
     table.dropColumn('type')
+  }).then()
+
+  await knex.schema.table('vote', (table) => {
     table.boolean('value')
   }).then()
 }
